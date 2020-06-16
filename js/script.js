92722e24ff33f4ed1113cd51afa283348a838247
Sara <saracorsob@gmail.com>
2020-06-16 16:11:26 +0200

    Aggiunta commenti js + stili css

--- js/script.js
+++ js/script.js
@@ -1,22 +1,30 @@
 $(document).ready(function() {
 
-
+  // Evento al click dell'icona invia
   $('.write-chat i.send').click(function() {
-    // alert('ok');
+
+    // Creo una variabile con il valore dell'input(messaggio)
     var valoreText = $('.write-chat input').val();
+    // Clono il template del messaggio
     var cloneText = $('.template li').clone();
 
+    // Creo la variabile con le ore e minuti correnti
     var d = new Date();
     var minutes = d.getMinutes();
     var hours = d.getHours();
     var currentTime = hours.toString() + ':' + minutes.toString();
 
+    // Scrivo all'interno del clone
     cloneText.children('p:first-child').text(valoreText);
     cloneText.children('p.time').text(currentTime);
 
+    // Aggiungo la classe css per dare gli stili
     cloneText.addClass('green-text');
+
+    // Appendo nell'html il clone del messaggio
     $('.read-chat ul').append(cloneText);
 
+    // Rimuovo il contenuto dall'input iniziale
     $('.write-chat input').val('');
 
   });
